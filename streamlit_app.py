8dd8a0e23840792776df9636d862c48a25029829
gautly000 <105822810+gautly000@users.noreply.github.com>
2022-05-20 14:17:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-Breakfast Menu
+streamlit.header('Breakfast Menu')
+strimlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
